b04748071fed40b466489481ad8d07ea8ffda01a
danielharary <76742502+danielharary@users.noreply.github.com>
2023-06-06 19:31:05 +0300

    Added new folders and made modifications

---
+++ backend/src/main.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+import uvicorn
+from pydantic import BaseModel
+
+app = FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+class TodoItem(BaseModel):
+    title: str
+    description: str | None = None
+
+db = []
+
+@app.get("/")
+async def root():
+    """ root route """
+    return {"message": "Hello World"}
+
+@app.post('/add')
+async def add_todo(item: TodoItem):
+    """ add todo """
+    db.append(item)
+    return item
+
+@app.get('/get')
+async def get_todos():
+    """ get all todos """
+    return db
+
+@app.delete('/remove')
+async def remove_todo(item: TodoItem):
+    db.remove(item)
+    return {"message": "Item removed successfully"}
+
+
+
+
+
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host='localhost', port=8000, reload=True)
